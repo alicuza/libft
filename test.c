52d4e1a32d1fb494741c741405abfe2b8d33def5
alicuza <stefan.ancuta@barbits.eu>
2025-11-27 12:25:38 +0100

    start of the printf flag behaviour tester

--- test.c
+++ test.c
@@ -11,12 +11,25 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdint.h>
+
+#define FLAG_MINUS	(1 << 0) /* bits: 00000001*/
+#define FLAG_ZERO	(1 << 1) /* bits: 00000010*/
+#define FLAG_HASH	(1 << 2) /* bits: 00000100*/
+#define FLAG_SPACE	(1 << 3) /* bits: 00001000*/
+#define FLAG_PLUS	(1 << 4) /* bits: 00010000*/
+
+typedef struct s_spec
+{
+	uint32_t	conv;
+	uint32_t	allowed_flags; /* bitmask */
+	uint32_t	allow_precision;
+}	t_spec;
+
+
+static const char g_flag_opts[] = "-0# +";
 
 int main(void)
 {
-	int c = -1;
-
-	while (++c < 256)
-		printf("%d: %c\n", c, c);
-	return 0;
+	
 }
