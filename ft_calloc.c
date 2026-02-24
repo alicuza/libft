7613847d173820a63a75aa9a3c4d3d2a81985c08
Stefan-Emanuel Ancuta <sancuta@c3r11p4.42vienna.com>
2026-02-24 13:01:55 +0100

    fixed calloc back, not sure how the ammend bs works tbf

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,29 +6,13 @@
 /*   By: sancuta <sancuta@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 22:39:35 by sancuta           #+#    #+#             */
-/*   Updated: 2026/02/24 12:13:27 by knajmech         ###   ########.fr       */
+/*   Updated: 2026/02/24 13:01:20 by sancuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <limits.h>
 
-
-void    *ft_calloc(size_t nmemb, size_t size)
-{
-    char    *ptr;
-
-    if (!nmemb || !size)
-        return (malloc(0));
-    if (nmemb && size > LLONG_MAX / nmemb)
-        return (0);
-    ptr = malloc(size * nmemb);
-    if (!ptr || !nmemb)
-        return (ptr);
-    ft_bzero(ptr, nmemb * size);
-    return (ptr);
-}
-/*void	*ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*res;
 	size_t	total_size;
@@ -47,4 +31,4 @@ void    *ft_calloc(size_t nmemb, size_t size)
 		return (NULL);
 	ft_bzero(res, total_size);
 	return (res);
-}*/
+}
