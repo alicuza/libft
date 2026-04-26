2fe4de948aa70490f94edb0449e0feb2f35e80f4
Stefan-Emanuel Ancuta <sancuta@c3r7p10.42vienna.com>
2026-04-26 14:06:04 +0200

    fixed the missing endif

--- minishell.h
+++ minishell.h
@@ -6,15 +6,18 @@
 /*   By: sancuta <sancuta@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 12:20:15 by sancuta           #+#    #+#             */
-/*   Updated: 2026/04/26 13:26:07 by sancuta          ###   ########.fr       */
+/*   Updated: 2026/04/26 13:57:25 by sancuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include <stdio.h> // for printf, perror
+# include <readline/readline.h>
 # include <readline/history.h>
 # include <stdlib.h> // for malloc, free, exit
 # include <unistd.h> // for access, write, read, fork, dup
-# include <fcntl.h> // for open, close, 
+# include <fcntl.h> // for open, close
 # include <sys/wait.h> // for wait, waitpid, wait3, wait4
+
+#endif
