a5461c63d75fb32b7353848c619f3ac865587829
Stefan-Emanuel Ancuta <sancuta@c2r5p12.42vienna.com>
2026-04-30 13:28:45 +0200

    added missing headers with comments of allowed functions

--- minishell.h
+++ minishell.h
@@ -6,18 +6,25 @@
 /*   By: sancuta <sancuta@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 12:20:15 by sancuta           #+#    #+#             */
-/*   Updated: 2026/04/26 13:57:25 by sancuta          ###   ########.fr       */
+/*   Updated: 2026/04/30 10:27:40 by sancuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-# include <stdio.h> // for printf, perror
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <stdlib.h> // for malloc, free, exit
-# include <unistd.h> // for access, write, read, fork, dup
-# include <fcntl.h> // for open, close
-# include <sys/wait.h> // for wait, waitpid, wait3, wait4
+# include <stdio.h>				// printf, perror
+# include <string.h>			// sterror
+# include <readline/readline.h>	// readline, rl_clear_history, rl_on_new_line, rl_replace_line, rl_redisplay
+# include <readline/history.h>	// add_history
+# include <stdlib.h>			// malloc, free, exit, getenv
+# include <unistd.h>			// access, write, read, fork, dup, dup2, pipe, getcwd, chdir, unlink, execve, isatty, ttyname, ttyslot
+# include <fcntl.h>				// open, close
+# include <signal.h>			// signal, sigaction, sigemptyset, sigaddset, kill
+# include <dirent.h>			// opendir, readdir, closedir
+# include <sys/wait.h>			// wait, waitpid, wait3, wait4
+# include <sys/stat.h>			// stat, lstat, fstat
+# include <sys/ioctl.h>			// ioctl
+# include <termios.h>			// tcgetattr, tcsetattr
+# include <termcap.h>			// tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
 
 #endif
