4731eca40557d267557f8f393dea001052bf08ec
Stefan-Emanuel Ancuta <sancuta@c3r2p4.42vienna.com>
2026-05-12 17:18:10 +0200

    removed the alignment as an arena member, it doesn't make sense

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: sancuta <sancuta@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 12:20:15 by sancuta           #+#    #+#             */
-/*   Updated: 2026/04/30 10:27:40 by sancuta          ###   ########.fr       */
+/*   Updated: 2026/05/12 17:14:03 by sancuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,22 @@
 # define MINISHELL_H
 # include <stdio.h>				// printf, perror
 # include <string.h>			// sterror
-# include <readline/readline.h>	// readline, rl_clear_history, rl_on_new_line, rl_replace_line, rl_redisplay
+# include <readline/readline.h>	// readline, rl_clear_history, rl_on_new_line,
+								// rl_replace_line, rl_redisplay
 # include <readline/history.h>	// add_history
 # include <stdlib.h>			// malloc, free, exit, getenv
-# include <unistd.h>			// access, write, read, fork, dup, dup2, pipe, getcwd, chdir, unlink, execve, isatty, ttyname, ttyslot
+# include <unistd.h>			// access, write, read, fork, dup, dup2, pipe,
+								// getcwd, chdir, unlink, execve,
+								// isatty, ttyname, ttyslot
 # include <fcntl.h>				// open, close
-# include <signal.h>			// signal, sigaction, sigemptyset, sigaddset, kill
+# include <signal.h>			// signal, sigaction, sigemptyset, sigaddset,
+								// kill
 # include <dirent.h>			// opendir, readdir, closedir
 # include <sys/wait.h>			// wait, waitpid, wait3, wait4
 # include <sys/stat.h>			// stat, lstat, fstat
 # include <sys/ioctl.h>			// ioctl
 # include <termios.h>			// tcgetattr, tcsetattr
-# include <termcap.h>			// tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+# include <termcap.h>			// tgetent, tgetflag, tgetnum, tgetstr, tgoto,
+								// tputs
 
 #endif
