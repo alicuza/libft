37ad8a9fb66155f6bd507a6c4926358e246258b5
Stefan-Emanuel Ancuta <sancuta@c3r2p4.42vienna.com>
2026-05-12 17:18:10 +0200

    removed the alignment as an arena member, it doesn't make sense

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: sancuta <sancuta@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 18:38:23 by sancuta           #+#    #+#             */
-/*   Updated: 2025/10/11 15:19:44 by sancuta          ###   ########.fr       */
+/*   Updated: 2026/05/12 17:11:10 by sancuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	if ((uintptr_t)dest > (uintptr_t)src
-        && (uintptr_t)src + n > (uintptr_t)dest)
+		&& (uintptr_t)src + n > (uintptr_t)dest)
 	{
 		i = n - 1;
 		while (i > 0)
