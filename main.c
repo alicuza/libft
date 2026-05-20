0a2ad52fdd247f6580d001be25e301b18ca02a48
alicuza <stefan.ancuta@barbits.eu>
2026-05-20 19:19:36 +0200

    first prototype entrypoint with readline and prompt

--- main.c
+++ main.c
@@ -11,11 +11,49 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+#define ARENA_SIZE 256
+#define CWD_SIZE 256
+
+char	*ft_get_prompt()
+{
+	char	*prompt;
+	char	cwd[CWD_SIZE];
+
+	prompt = ft_strjoin(getenv("USER"), "@");
+	prompt = ft_strjoin(prompt, getcwd(cwd, CWD_SIZE));
+	prompt = ft_strjoin(prompt, "$ ");
+	//TODO: error handling
+	return (prompt);
+}
+
+char	*ft_get_user_input(void)
+{
+	static char	*buffer;
+
+	if (buffer)
+	{
+		free(buffer);
+		buffer = NULL;
+	}
+	buffer = readline(ft_get_prompt());
+	if (buffer && *buffer)
+		add_history(buffer);
+
+	return (buffer);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
-	(void) argc;
-	(void) argv;
-	(void) envp;
+	(void)	argc;
+	(void)	argv;
+	(void)	envp;
+
+/*	t_env	env;
+
+	env->prompt = arena_init(ARENA_SIZE);
+*/
+
+	while (ft_get_user_input())
+		;
 	return (0);
 }
