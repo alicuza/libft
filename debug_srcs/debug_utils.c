a714a08a7fbaa50869ccd942926c0259d79303b1
Stefan-Emanuel Ancuta <fainica24@gmail.com>
2026-05-24 02:28:04 +0200

    generic name, need to figure out how to make it dynamic, depending on arena, shouldnt be that hard

--- debug_srcs/debug_utils.c
+++ debug_srcs/debug_utils.c
@@ -6,7 +6,7 @@
 /*   By: sancuta <sancuta@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 21:48:29 by sancuta           #+#    #+#             */
-/*   Updated: 2026/05/22 21:48:40 by sancuta          ###   ########.fr       */
+/*   Updated: 2026/05/23 22:59:43 by sancuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void  print_arena(t_arena *arena)
 {
-  printf("string_arena\n\tbuffer address = %p\n\toffset = %lu\n\tcapacity = %lu\n\tsentinel = %lu\n", arena->buf, arena->offset, arena->cap, arena->sentinel);
+  printf("arena\n\tbuffer address = %p\n\toffset = %lu\n\tcapacity = %lu\n\tsentinel = %lu\n", arena->buf, arena->offset, arena->cap, arena->sentinel);
   printf("head:\n");
   ft_print_memory(arena, sizeof(t_arena));
   printf("buffer:\n");
