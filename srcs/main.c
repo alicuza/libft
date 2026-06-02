aafbe4e2e98c8001073dafdefac65abb2e6f57ce
Stefan-Emanuel Ancuta <fainica24@gmail.com>
2026-06-02 17:26:17 +0200

    add first here-doc implementation, need to still test it out

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sancuta <sancuta@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 21:47:55 by sancuta           #+#    #+#             */
-/*   Updated: 2026/06/01 11:47:29 by sancuta          ###   ########.fr       */
+/*   Updated: 2026/06/02 16:14:31 by sancuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ static t_ctx	init_ctx(void)
 	t_ctx	c;
 
 	ft_memset(&c, 0, sizeof(t_ctx));
-	c.arena[AT_STRING] = arena_init(ARENA_SIZE, STR_SENTINEL_SIZE);
-	c.arena[AT_PROMPT] = arena_init(ARENA_SIZE, STR_SENTINEL_SIZE);
+	c.arena[AT_STRING] = arena_init(ARENA_SIZE, sizeof(char));
+	c.arena[AT_PROMPT] = arena_init(ARENA_SIZE, sizeof(char));
 	c.arena[AT_TOKEN] = arena_init(ARENA_SIZE, sizeof(t_token));
 	return (c);
 }
@@ -34,7 +34,7 @@ int	main(int argc, char **argv, char **envp)
 	c = init_ctx(envp);
 	while (true)
 	{
-		if (!get_user_input(&c))
+		if (!get_user_input(&c, DEFAULT_INPUT))
 			break ;
 #ifdef DEBUG
 		print_arena(&c.arena[AT_PROMPT]);
@@ -45,6 +45,8 @@ int	main(int argc, char **argv, char **envp)
 			token_idx = get_next_token_idx(&c);
 			if (!token_idx)
 				break;
+			if (!ft_strncmp(c.arena[AT_STRING].buf + get_token_from_idx(&c.arena[AT_TOKEN], token_idx)->content.start, "<<", 2))
+				get_user_input(&c, HERE_DOC);
 #ifdef DEBUG
 			print_token(&c, token_idx);
 			print_arena(&c.arena[AT_TOKEN]);
